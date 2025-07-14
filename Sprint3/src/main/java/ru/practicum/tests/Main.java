c9a5c39244389942f34a50f0b461a5b5272fba9c
Aleksandr Shipovalov <ashipovalov@gmail.com>
2025-07-14 20:22:22 +0300

    Update Main.java

--- Sprint3/src/main/java/ru/practicum/tests/Main.java
+++ Sprint3/src/main/java/ru/practicum/tests/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 System.out.println("Для демонстрации конфликта и стратегии разрешения оного");
         System.out.printf("Hello and welcome!");
-        System.out.println("Даешь конфликты");
+        System.out.println("Даешь конфликты и ура");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
