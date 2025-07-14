e0ce72c885a8b844908431164093c3fa82a9b8c5
Aleksandr Shipovalov <ashipovalov@gmail.com>
2025-07-14 20:17:17 +0300

    Добалена строчка для демонстрации конфликта

--- Sprint3/src/main/java/ru/practicum/tests/Main.java
+++ Sprint3/src/main/java/ru/practicum/tests/Main.java
@@ -3,6 +3,7 @@ package ru.practicum.tests;
 
 public class Main {
     public static void main(String[] args) {
+System.out.println("Для демонстрации конфликта и стратегии разрешения оного");
         System.out.printf("Hello and welcome!");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
