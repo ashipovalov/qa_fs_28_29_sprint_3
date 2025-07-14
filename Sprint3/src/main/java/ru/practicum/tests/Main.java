cb6fdea5af3c6d5376e4afad73b7c0439c423286
Aleskandr Shipovalov <ashipovalov@gmail.com>
2025-07-14 20:23:03 +0300

    Я хочу показать мердже стратегию

--- Sprint3/src/main/java/ru/practicum/tests/Main.java
+++ Sprint3/src/main/java/ru/practicum/tests/Main.java
@@ -6,6 +6,7 @@ public class Main {
 System.out.println("Для демонстрации конфликта и стратегии разрешения оного");
         System.out.printf("Hello and welcome!");
         System.out.println("Даешь конфликты и ура");
+        System.out.println("Даешь конфликты и не ура");
         for (int i = 1; i <= 5; i++) {
 System.out.println("Привет из консольного коммита");
             System.out.println("i = " + i);
