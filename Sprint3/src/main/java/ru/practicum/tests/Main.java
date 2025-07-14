f1feca8f0c8d39cafb39c0f4746a4f534834735c
Aleskandr Shipovalov <ashipovalov@gmail.com>
2025-07-14 20:28:15 +0300

    Коммит из IDE

--- Sprint3/src/main/java/ru/practicum/tests/Main.java
+++ Sprint3/src/main/java/ru/practicum/tests/Main.java
@@ -8,6 +8,7 @@ System.out.println("Для демонстрации конфликта и стр
         System.out.println("Даешь конфликты и ура");
         System.out.println("Даешь конфликты и не ура");
         for (int i = 1; i <= 5; i++) {
+            System.out.println("А это коммит из IDE");
 System.out.println("Привет из консольного коммита");
             System.out.println("i = " + i);
         }
